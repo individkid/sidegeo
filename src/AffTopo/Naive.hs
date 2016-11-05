c3b085767dddddb3779789229cbf3b2a981486a8
individkid <paulrrco@gmail.com>
2016-11-05 13:13:15 -0700

    license

--- src/AffTopo/Naive.hs
+++ src/AffTopo/Naive.hs
@@ -1,3 +1,18 @@
+--    AffTopo.Naive functions for symbolically manipulating affine planes
+--    Copyright (C) 2016  Paul Coelho
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module AffTopo.Naive where
 
@@ -50,6 +65,7 @@ subsets n a
 subset :: [Int] -> [a] -> [a]
 subset p a = foldl (\b q -> (a !! q) : b) [] p
 
+-- make elements sorted and unique
 welldef :: Ord a => [a] -> [a]
 welldef a = welldefF (sort a)
 
